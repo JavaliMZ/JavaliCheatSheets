3a444e0e2c9538bb92e8459bb5222baecf08fa66
JavaliMZ <syjulio123@gmail.com>
2021-08-15 14:08:24 +0100

    conver set to list for sorted list

--- cheatsheet/JavaliCheatSheet.py
+++ cheatsheet/JavaliCheatSheet.py
@@ -13,76 +13,84 @@ reset = "\033[0m"
 
 
 def signal_handler(sig, frame):
-	print("\n\n")
-	log.failure("You pressed Ctrl-C!...")
-	sys.exit(1)
+    print("\n\n")
+    log.failure("You pressed Ctrl-C!...")
+    sys.exit(1)
 
 
 def printCheat(cheatList, name):
-	printed = False
-	try:
-		for cheat in cheatList:
-			if name.strip().lower() in cheat.name.strip().lower():
-				print("\n\n")
-				log.success(f"{big + green}{cheat.name}{reset}\n\n")
-				print(cheat.output)
-				printed = True
-	except:
-		log.critical("Something wrong is not right...")
-		log.critical("Can't print this Cheat...")
-	if not printed:
-		log.failure(f"{big + red}Cheat not found!...{reset}")
-
+    printed = False
+    try:
+        for cheat in cheatList:
+            if name.strip().lower() in cheat.name.strip().lower():
+                print("\n\n")
+                log.success(f"{big + green}{cheat.name}{reset}\n\n")
+                print(cheat.output)
+                printed = True
+    except:
+        log.critical("Something wrong is not right...")
+        log.critical("Can't print this Cheat...")
+    if not printed:
+        log.failure(f"{big + red}Cheat not found!...{reset}")
 
 
 def valideOption(possibilities):
-	optionsIndex = [str(index) for index in range(0,len(possibilities))]
+    optionsIndex = [str(index) for index in range(0, len(possibilities))]
 
-	option = input(f"\n\tSelect a valide option... (Number):   \t").strip()
-	while option not in optionsIndex:
-		option = input(f"\t[{red}!{reset}]Select a {green}valide option... (Number){reset}:     \t").strip()
+    option = input(f"\n\tSelect a valide option... (Number):   \t").strip()
+    while option not in optionsIndex:
+        option = input(
+            f"\t[{red}!{reset}]Select a {green}valide option... (Number){reset}:     \t"
+        ).strip()
 
-	return possibilities[int(option)]
+    return possibilities[int(option)]
 
 
 def chooseCategory():
-	options = set()
-	for cheat in cheatList:
-		options.add(cheat.category)
-	
-	for index, option in enumerate(options):
-		log.info(f"{index} - {option}")
-	
-	return valideOption(list(options))
+    options = set()
+    for cheat in cheatList:
+        options.add(cheat.category)
+
+    options = list(options)
+    options.sort()
+
+    for index, option in enumerate(options):
+        log.info(f"{index} - {option}")
+
+    return valideOption(options)
+
 
 def chooseCheatName(category):
-	options = set()
-	for cheat in cheatList:
-		if cheat.category == category:
-			options.add(cheat.name)
-	
-	for index, option in enumerate(options):
-		log.info(f"{index} - {option}")
-	
-	return valideOption(list(options))
+    options = set()
+    for cheat in cheatList:
+        if cheat.category == category:
+            options.add(cheat.name)
+
+    options = list(options)
+    options.sort()
+
+    for index, option in enumerate(options):
+        log.info(f"{index} - {option}")
+
+    return valideOption(options)
 
 
 def manualSelection():
-	category = None
-	name = None
+    category = None
+    name = None
 
-	category = chooseCategory()
-	cheatName = chooseCheatName(category)
-	printCheat(cheatList, cheatName)
+    category = chooseCategory()
+    cheatName = chooseCheatName(category)
+    printCheat(cheatList, cheatName)
 
 
 def main():
-	signal.signal(signal.SIGINT, signal_handler)
+    signal.signal(signal.SIGINT, signal_handler)
 
-	if len(sys.argv) == 1:
-		manualSelection()
-	if len(sys.argv) == 2:
-		printCheat(cheatList, sys.argv[1])
+    if len(sys.argv) == 1:
+        manualSelection()
+    if len(sys.argv) == 2:
+        printCheat(cheatList, sys.argv[1])
 
 
 main()
