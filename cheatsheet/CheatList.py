acedd6efbbd31704de47ee16b0a2394e50e4854c
Sjulio123 <syjulio123@gmail.com>
2021-08-16 14:36:44 +0100

    Add color for cheat.output (gray and underline for title, gray for commentaries). Add a blue box envolving  the cheat on printCheat()

--- cheatsheet/CheatList.py
+++ cheatsheet/CheatList.py
@@ -1,12 +1,13 @@
 class Cheat:
-    def __init__(self, name):
-        self.name = name
-        self.category = None
-        self.output = None
+	def __init__(self, name):
+		self.name = name
+		self.category = None
+		self.output = None
+		self.addToList()
 
-    def addToList(self):
-        global cheatList
-        cheatList.append(self)
+	def addToList(self):
+		global cheatList
+		cheatList.append(self)
 
 
 cheatList = []
@@ -16,14 +17,13 @@ cheatList = []
 
 PSCredential = Cheat("PSCredential")
 PSCredential.category = "Windows"
-PSCredential.output = """\t[*] Create a Credential Object for PowerShell:
+PSCredential.output = """[*] Create a Credential Object for PowerShell:
 
 $user = 'user'
 $pw = 'password'
 $secure_pw = ConvertTo-SecureString $pw -AsPlainText -Force
 $cred = New-Object System.Management.Automation.PSCredential $user, $secure_pw
 Invoke-Command -ComputerName localhost -Credential $cred -ScriptBlock { whoami }"""
-PSCredential.addToList()
 
 ######################################
 ######################################
@@ -31,7 +31,17 @@ PSCredential.addToList()
 Test = Cheat("PSCredenpois")
 Test.category = "Linux"
 Test.output = "Isto é só um teste..."
-Test.addToList()
 
 ######################################
 ######################################
+
+icmp_reverse_shell_windows = Cheat("ICMP - Reverse Shell Windows (Nishang)")
+icmp_reverse_shell_windows.category = "Windows"
+icmp_reverse_shell_windows.output = """[*] Reverse Shell when TCP and UDP connection are blocked by Firewall rules.
+
+# Download Nishang
+# Remove all blank lines and commented lines for no bugs
+# convert into base64 encoded string
+
+
+"""
--- cheatsheet/JavaliCheatSheet.py
+++ cheatsheet/JavaliCheatSheet.py
@@ -14,22 +14,24 @@ red = "\033[31m"
 green = "\033[32m"
 yellow = "\033[33m"
 blue = "\033[34m"
+gray = "\033[90m"
 reset = "\033[0m"
+underline = "\033[4m"
 CURSOR_UP_ONE = "\x1b[1A"
 ERASE_LINE = "\x1b[2K"
 
 
 def signal_handler(sig, frame):
-    sys.exit(1)
+	sys.exit(1)
 
 
 def eraseLastPrintedLine():
-    print(CURSOR_UP_ONE + ERASE_LINE + CURSOR_UP_ONE)
+	print(CURSOR_UP_ONE + ERASE_LINE + CURSOR_UP_ONE)
 
 
 def banner():
-    print(
-        f"""{yellow}
+	print(
+		f"""{yellow}
 :'######::'##::::'##:'########::::'###::::'########::'######::'##::::'##:'########:'########:'########::'######::
 '##... ##: ##:::: ##: ##.....::::'## ##:::... ##..::'##... ##: ##:::: ##: ##.....:: ##.....::... ##..::'##... ##:
  ##:::..:: ##:::: ##: ##::::::::'##:. ##::::: ##:::: ##:::..:: ##:::: ##: ##::::::: ##:::::::::: ##:::: ##:::..::
@@ -39,93 +41,103 @@ def banner():
 . ######:: ##:::: ##: ########: ##:::: ##:::: ##::::. ######:: ##:::: ##: ########: ########:::: ##::::. ######::
 :......:::..:::::..::........::..:::::..:::::..::::::......:::..:::::..::........::........:::::..::::::......:::                                                                                                    	
 {reset}"""
-    )
+	)
 
 
 def printCheat(cheatList, name):
-    printed = False
-    try:
-        for cheat in cheatList:
-            if name.strip().lower() in cheat.name.strip().lower():
-                maxLen = [len(line) for line in cheat.output.split("\n")]
-                maxLen.sort()
-                print(f"\n{blue}{'▓' * (maxLen[-1] + 4) }{reset}")
-                print(f"{blue}▓ {reset}")
-                print(f"{blue}▓ {reset}", end="")
-                log.success(f"{big + green}{cheat.name}{reset}\n")
-                print(f"{blue}▓ {reset}")
+	printed = False
+	try:
+		for cheat in cheatList:
+			if name.strip().lower() in cheat.name.strip().lower():
+				maxLen = [len(line) for line in cheat.output.split("\n")]
+				maxLen.sort()
+				print(f"\n{blue}{'▓' * (maxLen[-1] + 4) }{reset}")
+				print(f"{blue}▓ {reset}")
+				print(f"{blue}▓ {reset}", end="")
+				log.success(f"{big + green}{cheat.name}{reset}\n")
+				print(f"{blue}▓ {reset}")
 
-                for line in cheat.output.split("\n"):
-                    print(f"{blue}▓ {reset}{line}")
+				for line in cheat.output.split("\n"):
+					try:
+						commented = line.split()[0] == "#"
+						title = line.split()[0] == "[*]"
+						if commented:
+							print(f"{blue}▓ {big + gray}{line}{reset}")
+						elif title:
+							print(f"{blue}▓ {big + underline + gray}{line}{reset}")
+						else:
+							print(f"{blue}▓ {reset}{line}")
+					except:
+						print(f"{blue}▓ {reset}{line}")
 
-                print(f"{blue}▓ {reset}")
-                print(f"{blue}{'▓' * (maxLen[-1] + 4) }{reset}\n\n")
-                printed = True
-    except:
-        log.critical("Something wrong is not right...")
-        log.critical("Can't print this Cheat...")
-    if not printed:
-        log.failure(f"{big + red}Cheat not found!...{reset}")
+				print(f"{blue}▓ {reset}")
+				print(f"{blue}{'▓' * (maxLen[-1] + 4) }{reset}\n\n")
+				printed = True
+	except:
+		log.critical("Something wrong is not right...")
+		log.critical("Can't print this Cheat...")
+	if not printed:
+		log.failure(f"{big + red}Cheat not found!...{reset}")
 
 
 def valideOption(possibilities):
-    optionsIndex = [str(index) for index in range(0, len(possibilities))]
+	optionsIndex = [str(index) for index in range(0, len(possibilities))]
 
-    option = input(f"\n\tSelect a valide option... (Number):   \t").strip()
-    eraseLastPrintedLine()
-    while option not in optionsIndex:
-        option = input(
-            f"\t[{red}!{reset}]Select a {green}valide option... (Number){reset}:     \t"
-        ).strip()
-        eraseLastPrintedLine()
+	option = input(f"\n\tSelect a valide option... (Number):   \t").strip()
+	eraseLastPrintedLine()
+	while option not in optionsIndex:
+		option = input(
+			f"\t[{red}!{reset}]Select a {green}valide option... (Number){reset}:     \t"
+		).strip()
+		eraseLastPrintedLine()
 
-    return possibilities[int(option)]
+	return possibilities[int(option)]
 
 
 def printOptions(category=None):
-    options = set()
-    for cheat in cheatList:
-        if category == None:
-            options.add(cheat.category)
-        if cheat.category == category:
-            options.add(cheat.name)
+	options = set()
+	for cheat in cheatList:
+		if category == None:
+			options.add(cheat.category)
+		if cheat.category == category:
+			options.add(cheat.name)
 
-    options = list(options)
-    options.sort()
+	options = list(options)
+	options.sort()
 
-    if category == None:
-        log.success(f"Select the category you want: \n\n")
-    else:
-        log.success(f"Select the wanted CheatSheet in {category.upper()} category.\n\n")
+	if category == None:
+		log.success(f"Select the category you want: \n\n")
+	else:
+		log.success(f"Select the wanted CheatSheet in {category.upper()} category.\n\n")
 
-    for index, option in enumerate(options):
-        print("\t", end="")
-        log.info(f"{index} - {option}")
+	for index, option in enumerate(options):
+		print("\t", end="")
+		log.info(f"{index} - {option}")
 
-    return valideOption(options)
+	return valideOption(options)
 
 
 def manualSelection():
-    category = None
-    name = None
+	category = None
+	name = None
 
-    category = printOptions()
-    cheatName = printOptions(category)
-    printCheat(cheatList, cheatName)
+	category = printOptions()
+	cheatName = printOptions(category)
+	printCheat(cheatList, cheatName)
 
 
 def main():
-    signal.signal(signal.SIGINT, signal_handler)
-    banner()
+	signal.signal(signal.SIGINT, signal_handler)
+	banner()
 
-    if len(sys.argv) == 1:
-        manualSelection()
-        while True:
-            input("Press any key to continue...\nCtrl-C to exit...")
-            manualSelection()
+	if len(sys.argv) == 1:
+		manualSelection()
+		while True:
+			input("Press any key to continue...\nCtrl-C to exit...")
+			manualSelection()
 
-    if len(sys.argv) == 2:
-        printCheat(cheatList, sys.argv[1])
+	if len(sys.argv) == 2:
+		printCheat(cheatList, sys.argv[1])
 
 
 main()
